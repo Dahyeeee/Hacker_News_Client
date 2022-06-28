bfd6333103bc3f18887659dfa49a6f839f39e5a7
Dahye <06robin11@gmail.com>
2022-06-28 21:37:44 +0900

    implemented next page, previous page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const chosenArt = document.createElement("div");
 
+const store = {
+  currentPage: 1,
+};
+
 const getData = (url) => {
   ajax.open("GET", url, false);
   ajax.send();
@@ -15,21 +19,34 @@ const newsFeedPage = () => {
   const newsFeed = getData(NEWS_URL);
 
   const newsList = ["<ul>"];
-  for (let i = 0; i < newsFeed.length; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-      <a href='#${newsFeed[i].id}'>
+      <a href='#/show/${newsFeed[i].id}'>
         ${newsFeed[i].title} (${newsFeed[i].comments_count}
       </a>
-    </li>`);
+    </li>
+   `);
   }
   newsList.push("</ul>");
+  newsList.push(`
+  <div>
+    <a href='#/page/${
+      store.currentPage > 1 ? store.currentPage - 1 : 1
+    }'>이전 페이지</a> 
+    <a href='#/page/${
+      Math.ceil(newsFeed.length / 10) > store.currentPage
+        ? store.currentPage + 1
+        : store.currentPage
+    }'>다음 페이지</a>
+  </div>`);
 
   container.innerHTML = newsList.join("");
 };
 
 const articlePage = () => {
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
+  console.log(id);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
   console.log(newsContent);
 
@@ -38,7 +55,7 @@ const articlePage = () => {
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href=#>목록으로</a>
+      <a href=#/page/${store.currentPage}>목록으로</a>
     </div>`;
 };
 
@@ -46,8 +63,11 @@ function router() {
   const routePath = location.hash;
   console.log(routePath);
   if (routePath === "") newsFeedPage();
-  else articlePage();
+  else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = +routePath.substring(7);
+    newsFeedPage();
+  } else articlePage();
 }
 
-window.addEventListener("hashchange", router);
+window.addEventListener("hashchange", router());
 router();
--- dist/index.85fc00b3.js
+++ dist/index.85fc00b3.js
@@ -537,6 +537,9 @@ const ajax = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const chosenArt = document.createElement("div");
+const store = {
+    currentPage: 1
+};
 const getData = (url)=>{
     ajax.open("GET", url, false);
     ajax.send();
@@ -547,33 +550,43 @@ const newsFeedPage = ()=>{
     const newsList = [
         "<ul>"
     ];
-    for(let i = 0; i < newsFeed.length; i++)newsList.push(`
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++)newsList.push(`
     <li>
-      <a href='#${newsFeed[i].id}'>
+      <a href='#/show/${newsFeed[i].id}'>
         ${newsFeed[i].title} (${newsFeed[i].comments_count}
       </a>
-    </li>`);
+    </li>
+   `);
     newsList.push("</ul>");
+    newsList.push(`
+  <div>
+    <a href='#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}'>이전 페이지</a> 
+    <a href='#/page/${Math.ceil(newsFeed.length / 10) > store.currentPage ? store.currentPage + 1 : store.currentPage}'>다음 페이지</a>
+  </div>`);
     container.innerHTML = newsList.join("");
 };
 const articlePage = ()=>{
-    const id = location.hash.substring(1);
+    const id = location.hash.substring(7);
+    console.log(id);
     const newsContent = getData(CONTENT_URL.replace("@id", id));
     console.log(newsContent);
     container.innerHTML = "";
     container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href=#>목록으로</a>
+      <a href=#/page/${store.currentPage}>목록으로</a>
     </div>`;
 };
 function router() {
     const routePath = location.hash;
     console.log(routePath);
     if (routePath === "") newsFeedPage();
-    else articlePage();
+    else if (routePath.indexOf("#/page/") >= 0) {
+        store.currentPage = +routePath.substring(7);
+        newsFeedPage();
+    } else articlePage();
 }
-window.addEventListener("hashchange", router);
+window.addEventListener("hashchange", router());
 router();
 
 },{}]},["lrGur","99uh0"], "99uh0", "parcelRequire94c2")
